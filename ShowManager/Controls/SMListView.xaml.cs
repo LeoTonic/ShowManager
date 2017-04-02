034d278c7a1526e6147f9e0e856a2294bdab6343
Anton Leonov <a.leonov80@gmail.com>
2017-04-03 01:38:43 +0500

    Develop list box 2017-04-03

--- ShowManager/Controls/SMListView.xaml.cs
+++ ShowManager/Controls/SMListView.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,7 +25,21 @@ namespace ShowManager.Controls
 		public ObservableCollection<SMListViewItem> Items { get; set; }
 		private ListBoxItem draggedItem = null;
 		private DragDropWindow ddRef = null;
+
 		private readonly Style listStyle = null;
+		private QueryContinueDragEventHandler queryHandler;
+
+		private bool isDragging = false;
+		private Point dragPoint;
+		private Vector dragVector;
+		private bool isMouseLeft = false;
+
+		private Point selectPoint;
+		private Rect selectRect = new Rect();
+
+		private Point itemPoint;
+		private Point nullPoint = new Point();
+		private Rect itemRect = new Rect();
 
 		public SMListView()
 		{
@@ -41,19 +56,83 @@ namespace ShowManager.Controls
 			listStyle = new Style(typeof(ListBoxItem));
 			listStyle.Setters.Add(new Setter(ListBoxItem.AllowDropProperty, true));
 			listStyle.Setters.Add(new EventSetter(ListBoxItem.PreviewMouseRightButtonDownEvent, new MouseButtonEventHandler(SMListView_ItemPreviewMouseRightDown)));
+			listStyle.Setters.Add(new EventSetter(ListBoxItem.PreviewMouseLeftButtonDownEvent, new MouseButtonEventHandler(SMListView_ItemMouseDown)));
+			listStyle.Setters.Add(new EventSetter(ListBoxItem.PreviewMouseLeftButtonUpEvent, new MouseButtonEventHandler(SMListView_ItemMouseUp)));
 			listStyle.Setters.Add(new EventSetter(ListBoxItem.GiveFeedbackEvent, new GiveFeedbackEventHandler(SMListView_GiveFeedback)));
 			listStyle.Setters.Add(new EventSetter(ListBoxItem.DropEvent, new DragEventHandler(SMListViewItem_Drop)));
 			ListViewControl.ItemContainerStyle = listStyle;
+
+			queryHandler = new QueryContinueDragEventHandler(ItemQueryContinueDrag);
+
+			ListViewControl.Drop += ListViewControl_Drop;
+			ListViewControl.MouseLeftButtonDown += ListViewControl_MouseLeftButtonDown;
+			ListViewControl.MouseLeftButtonUp += ListViewControl_MouseLeftButtonUp;
+			ListViewControl.MouseMove += ListViewControl_MouseMove;
+
 			DataContext = this;
+		}
+
+		private void ListViewControl_MouseMove(object sender, MouseEventArgs e)
+		{
+			dragVector = dragPoint - e.GetPosition(null);
+			if ((Math.Abs(dragVector.X) > SystemParameters.MinimumHorizontalDragDistance || Math.Abs(dragVector.Y) > SystemParameters.MinimumVerticalDragDistance) && !isDragging && draggedItem != null)
+			{
+				if (ddRef != null)
+				{
+					ddRef.ShowWindow();
+				}
+				draggedItem.QueryContinueDrag += queryHandler;
+				DragDrop.DoDragDrop(draggedItem, draggedItem.DataContext, DragDropEffects.Move);
+				isDragging = true;
+			}
+
+			if (isMouseLeft)
+			{
+				Point curPoint = e.GetPosition(ListViewControl);
+				UpdateDragSelectionRect(selectPoint, curPoint);
+			}
+		}
+
+		private void ListViewControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			isMouseLeft = true;
+			selectPoint = e.GetPosition(ListViewControl);
+			DragSelectionCanvas.Visibility = Visibility.Visible;
+			UpdateDragSelectionRect(selectPoint, selectPoint);
+
+			ListViewControl.CaptureMouse();
+			e.Handled = true;
+		}
+
+		private void ListViewControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			isMouseLeft = false;
+			DragSelectionCanvas.Visibility = Visibility.Collapsed;
+			ListViewControl.ReleaseMouseCapture();
+			e.Handled = true;
+		}
+
+		private void SMListView_ItemMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			ListViewControl_MouseLeftButtonDown(sender, e);
+		}
+		private void SMListView_ItemMouseUp(object sender, MouseButtonEventArgs e)
+		{
+			ListViewControl_MouseLeftButtonUp(sender, e);
+		}
+
+		private void ListViewControl_Drop(object sender, DragEventArgs e)
+		{
+			EndDrag();
 		}
 
 		protected void SMListView_ItemPreviewMouseRightDown(object sender, MouseButtonEventArgs e)
 		{
 			EndDrag();
-
 			if (sender is ListBoxItem)
 			{
 				draggedItem = sender as ListBoxItem;
+				dragPoint = e.GetPosition(null);
 				//var card = draggedItem.DataContext as SMListViewItem;
 
 				//card.fromBoxControl = this;
@@ -61,12 +140,6 @@ namespace ShowManager.Controls
 				//draggedItem.IsSelected = true;
 
 				// create the visual feedback drag and drop item
-				if (ddRef != null)
-				{
-					ddRef.ShowWindow();
-				}
-
-				DragDrop.DoDragDrop(draggedItem, draggedItem.DataContext, DragDropEffects.Move);
 			}
 		}
 
@@ -83,6 +156,17 @@ namespace ShowManager.Controls
 			}
 		}
 
+		private void ItemQueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+		{
+			if (e.KeyStates == DragDropKeyStates.None)
+			{
+				if (draggedItem != null)
+				{
+					draggedItem.QueryContinueDrag -= queryHandler;
+				}
+				EndDrag();
+			}
+		}
 		protected void SMListViewItem_Drop(object sender, DragEventArgs e)
 		{
 			e.Handled = true;
@@ -100,6 +184,47 @@ namespace ShowManager.Controls
 			{
 				ddRef.HideWindow();
 			}
+			isDragging = false;
 		}
+
+		private void UpdateDragSelectionRect(Point pt1, Point pt2)
+		{
+			selectRect.X = Math.Min(pt1.X, pt2.X);
+			selectRect.Y = Math.Min(pt1.Y, pt2.Y);
+			selectRect.Width = Math.Abs(pt1.X - pt2.X);
+			selectRect.Height = Math.Abs(pt1.Y - pt2.Y);
+
+			Canvas.SetLeft(DragSelectionBorder, selectRect.X);
+			Canvas.SetTop(DragSelectionBorder, selectRect.Y);
+			DragSelectionBorder.Width = selectRect.Width;
+			DragSelectionBorder.Height = selectRect.Height;
+
+			// Выборка элементов
+			foreach(object lvi in ListViewControl.Items)
+			{
+				var smi = lvi as SMListViewItem;
+				itemPoint = smi.TranslatePoint(nullPoint, ListViewControl);
+				itemRect.X = itemPoint.X;
+				itemRect.Y = itemPoint.Y;
+				itemRect.Width = smi.ActualWidth;
+				itemRect.Height = smi.ActualHeight;
+
+				if (selectRect.IntersectsWith(itemRect))
+				{
+					if (!ListViewControl.SelectedItems.Contains(lvi))
+					{
+						ListViewControl.SelectedItems.Add(lvi);
+					}
+				}
+				else
+				{
+					if (ListViewControl.SelectedItems.Contains(lvi))
+					{
+						ListViewControl.SelectedItems.Remove(lvi);
+					}
+				}
+			}
+		}
+
 	}
 }
