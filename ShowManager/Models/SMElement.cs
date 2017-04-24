126327f8af7c54592d3ba870286aebd5870d3f15
AnthonyLeonoff <a.leonov80@gmail.com>
2017-04-24 20:26:57 +0500

    2017-04-24 save element

--- ShowManager/Models/SMElement.cs
+++ ShowManager/Models/SMElement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -75,5 +76,32 @@ namespace ShowManager.Models
 			this.id = DateTime.Now.Ticks;
 		}
 
+		protected bool Save(StreamWriter sw)
+		{
+			try
+			{
+				sw.WriteLine("SM_ELEMENT:");
+				sw.WriteLine(ID.ToString());
+				sw.WriteLine(Name);
+				sw.WriteLine(ImageKey.ToString());
+			}
+			catch (IOException ioex)
+			{
+				System.Diagnostics.Debug.WriteLine(ioex.Message);
+				return false;
+			}
+			return true;
+		}
+
+		/*
+		 * // Example #4: Append new text to an existing file.
+        // The using statement automatically flushes AND CLOSES the stream and calls 
+        // IDisposable.Dispose on the stream object.
+        using (System.IO.StreamWriter file = 
+            new System.IO.StreamWriter(@"C:\Users\Public\TestFolder\WriteLines2.txt", true))
+        {
+            file.WriteLine("Fourth line");
+        }
+		 */
 	}
 }
